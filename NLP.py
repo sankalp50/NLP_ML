7b1dbb805b3840401b83559afd25a13b2a84abae
sankalp50 <37979821+sankalp50@users.noreply.github.com>
2020-03-28 01:14:43 +0530

    Add files via upload

--- NLP.py
+++ NLP.py
@@ -1,1 +1,48 @@
+#Natural Language Processing
 
+#Importing the libraries
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+#Importing the dataset
+dataset=pd.read_csv("Restaurant_Reviews.tsv",delimiter='\t',quoting=3)
+
+#Cleaning the texts
+import re
+import nltk
+nltk.download('stopwords')
+from nltk.corpus import stopwords
+from nltk.stem.porter import PorterStemmer
+ps=PorterStemmer()
+corpus=[]
+for i in range(0,1000,1):
+    review=re.sub('[^a-zA-Z]',' ',dataset['Review'][i])
+    review=review.lower()
+    review=review.split()
+    review=[ps.stem(word) for word in review if not word in set(stopwords.words('english'))]
+    review=' '.join(review)
+    corpus.append(review)
+
+#Creating Bag of Words model
+from sklearn.feature_extraction.text import CountVectorizer
+cv=CountVectorizer(max_features=1000)
+x=cv.fit_transform(corpus).toarray()
+y=dataset.iloc[:, 1].values
+
+#splitting dataset into training set and test set
+from sklearn.cross_validation import train_test_split
+x_train,x_test,y_train,y_test=train_test_split(x,y,test_size=0.20,random_state=0)
+
+#fitting the training dataset into classifier model
+from sklearn.naive_bayes import GaussianNB
+classifier= GaussianNB()
+classifier.fit(x_train,y_train)
+
+#predicting the test dataset results
+y_pred=classifier.predict(x_test)
+
+#making the confusion matrix
+from sklearn.metrics import confusion_matrix
+cm=confusion_matrix(y_test,y_pred)
+    
